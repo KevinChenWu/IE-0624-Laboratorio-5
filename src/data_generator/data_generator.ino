9fabae78ac4c7096cfea02947d70c5ad1f4d3680
KevinChenWu <hongwenchen.k.c.w.90@gmail.com>
2023-02-25 02:00:32 -0600

    Implementación completa de lectura del LSM9DS1

--- src/data_generator/data_generator.ino
+++ src/data_generator/data_generator.ino
@@ -1,30 +1,51 @@
 #include <Arduino_LSM9DS1.h>
 
+const float noise_threshold = 2.2;
+const int max_samples = 1024;
+volatile int timestamp = 0;
+volatile int num_sample = max_samples;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   while(!Serial);
   IMU.begin();
-  Serial.println("aX,aY,aZ,gX,gY,gZ");
+  Serial.println("timestamp,aX,aY,aZ,gX,gY,gZ");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   float aX, aY, aZ, gX, gY, gZ;
-  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
-    IMU.readAcceleration(aX, aY, aZ);
-    IMU.readGyroscope(gX, gY, gZ);
-    Serial.print(aX, 4);
-    Serial.print(",");
-    Serial.print(aY, 4);
-    Serial.print(",");
-    Serial.print(aZ, 4);
-    Serial.print(",");
-    Serial.print(gX, 4);
-    Serial.print(",");
-    Serial.print(gY, 4);
-    Serial.print(",");
-    Serial.print(gZ, 4);
-    Serial.println();
+  while (num_sample == max_samples) {
+    if (IMU.accelerationAvailable()) {
+      IMU.readAcceleration(aX, aY, aZ);
+      float aceleration = fabs(aX) + fabs(aY) + fabs(aZ);
+      if (aceleration >= noise_threshold) {
+        num_sample = 0;
+      }
+    }
+  }
+
+  while (num_sample < max_samples) {
+    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+      IMU.readAcceleration(aX, aY, aZ);
+      IMU.readGyroscope(gX, gY, gZ);
+      Serial.print(timestamp);
+      Serial.print(",");
+      Serial.print(aX, 4);
+      Serial.print(",");
+      Serial.print(aY, 4);
+      Serial.print(",");
+      Serial.print(aZ, 4);
+      Serial.print(",");
+      Serial.print(gX, 4);
+      Serial.print(",");
+      Serial.print(gY, 4);
+      Serial.print(",");
+      Serial.print(gZ, 4);
+      Serial.println();
+      timestamp++;
+      num_sample++;
+    }
   }
 }
